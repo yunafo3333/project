4d89e33ba3a1bc2b0f71f16794a20523a3706e55
yunafo3333 <66707666+yunafo3333@users.noreply.github.com>
2020-06-10 11:55:11 +0900

    dior_pj
    dior poroject

---
+++ second/dior/js/script.js
@@ -0,0 +1,7 @@
+
+//color change
+//var color = '#';
+//var letters = ['621620', '623e31', '8a7759', '040a0d', 'aa715d', '040a0d', '5e5f67', 'c37070', 'eae160', 'bf7aa3', 'd7d967'];
+//
+//color += letters[Math.floor(Math.random() * letters.length)];
+//document.getElementById('c-img').style.background = color;
