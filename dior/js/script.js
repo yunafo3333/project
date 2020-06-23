5149a202f07be384961418e40b2757b2dd01eb12
lee <lee@leeui-MacBook-Air.local>
2020-06-23 11:59:39 +0900

    dior_0623

--- dior/js/script.js
+++ dior/js/script.js
@@ -1,3 +1,22 @@
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    console.log(scroll);
+    if (scroll >= 50) {
+        $(".go-top").addClass("show");
+    } else {
+        $(".go-top").removeClass("show");
+    }
+})
+
+$('a').click(function(){
+    $('body').animate({
+    screenTop: $(this).attr('href').offset().top
+}, 1000);
+});
+
+
 $(document).on('click', 'a[href="#"]', function(e) {
     e.preventDefault();
 });
+
+
