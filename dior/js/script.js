ff759ec237bf3e63c5695187e4fe8f26eb896c28
lee <lee@leeui-MacBook-Air.local>
2020-06-24 16:19:29 +0900

    dior_0624

--- dior/js/script.js
+++ dior/js/script.js
@@ -20,3 +20,32 @@ $(document).on('click', 'a[href="#"]', function(e) {
 });
 
 
+// slide
+
+
+$('ul.indicator li a').on('click', function() {
+    var index = $('ul.indicator li').index($(this).parent());
+    // var index = $(this).parent().index();
+    showSlide(index + 1);
+});
+
+$('p.control a.prev').on('click', function() {
+    var index = $('ul.indicator li').index($('ul.indicator li.on'));
+    // var index = $('ul.indicator li.on').index();
+    index = (index <= 0) ? 3 : (index - 1);
+    showSlide(index + 1);
+});
+
+$('p.control a.next').on('click', function() {
+    var index = $('ul.indicator li').index($('ul.indicator li.on'));
+    // var index = $('ul.indicator li.on').index();
+    index = (index >= 3) ? 0 : (index + 1);
+    showSlide(index + 1);
+});
+
+function showSlide(n) {
+    $('ul.slide li').css({'display': 'none'});
+    $('ul.slide li:eq(' + (n - 1) + ')').css({'display': 'block'});
+    $('ul.indicator li').removeClass('on');
+    $('ul.indicator li:eq(' + (n - 1) + ')').addClass('on');
+}
---
+++ dior/js/slide_script.js
@@ -0,0 +1,43 @@
+$(function(){
+  console.log('start');
+  
+  var numSlide = $('ul.slide li').length;
+  var slideNow = 0;
+  var slidePrev = 0;
+  var slideNext = 0;
+  var effect = 'none';
+  
+  showSlide(1);
+  
+  $('ul.indicator li a').on('click', function() {
+      var index = $('ul.indicator li').index($(this).parent());
+      showSlide(index + 1);
+  });
+  $('p.control a.prev').on('click', function() {
+      showSlide(slidePrev);
+  });
+  $('p.control a.next').on('click', function() {
+      showSlide(slideNext);
+  });
+  
+  function showSlide(n) {
+      if (effect === 'fade') {
+          if (slideNow === n) return false;
+          $('ul.slide li:eq(' + (slideNow - 1) + ')').stop(true).animate({'opacity': 0}, 500, function() {
+              $(this).css({'display': 'none'});
+          });
+          $('ul.slide li:eq(' + (n - 1) + ')').css({'display': 'block', 'opacity': 0}).stop(true).animate({'opacity': 1}, 500);
+      } else {
+          $('ul.slide li').css({'display': 'none'});
+          $('ul.slide li:eq(' + (n - 1) + ')').css({'display': 'block'});
+      }
+      $('ul.indicator li').removeClass('on');
+      $('ul.indicator li:eq(' + (n - 1) + ')').addClass('on');
+      slideNow = n;
+      slidePrev = (n <= 1) ? numSlide : (n - 1);
+      slideNext = (n >= numSlide) ? 1 : (n + 1);
+      //console.log(slidePrev + ' / ' + slideNow + ' / ' + slideNext);
+  }
+  
+}
+)
