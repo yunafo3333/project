3b9d8a491cf67d58f6f79111bb6cb63e4744d1aa
lee <lee@leeui-MacBook-Air.local>
2020-06-27 22:38:12 +0900

    0627

--- bang-olufsen/js/gnb_common.js
+++ bang-olufsen/js/gnb_common.js
@@ -5,12 +5,18 @@ $(window).on('scroll', function() {
 
 function checkHeader() {
     var scrollAmt = $(document).scrollTop();
-    if (scrollAmt > 120) {
+    if (scrollAmt > 200) {
         $('#header').addClass('white');
-        $(".support").attr("src","https://www.bang-olufsen.com/static/1.4.0.1524/images/common/icon_support.svg"); 
+        $(".support").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/icon_support_black.svg"); 
+        $(".store").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/icon_store_black.svg"); 
+        $(".cart").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/cart_black.svg");  
+        $(".primary-logo").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/primary_logo_black.svg"); 
     } else {
         $('#header').removeClass('white');
-        $(".support").removeattr(); 
+        $(".support").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/icon_support_white.svg");
+        $(".store").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/icon_store_white.svg"); 
+        $(".cart").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/cart-white.svg");  
+        $(".primary-logo").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/primary-logo-white.svg"); 
     }
 
 
---
+++ bang-olufsen/js/script.js
@@ -0,0 +1,7 @@
+
+
+$(document).on('click', 'a[href="#"]', function(e) {
+    e.preventDefault();
+});
+
+
