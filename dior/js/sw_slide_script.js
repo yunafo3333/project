0d6e3ef2ec24ced79f86d3005f2a8771e7813791
lee <lee@leeui-MacBook-Air.local>
2020-06-29 10:14:39 +0900

    06_29_bang

--- dior/js/sw_slide_script.js
+++ dior/js/sw_slide_script.js
@@ -23,6 +23,11 @@ $(function () {
                 el: '.swiper-pagination',
                 clickable: true,
             },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+              },
 
         });
+
 })
