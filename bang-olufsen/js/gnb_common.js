ea9de946dc51cacb15b7447bb22877ef7e97b384
lee <lee@leeui-MacBook-Air.local>
2020-07-04 23:19:35 +0900

    all_0704

--- bang-olufsen/js/gnb_common.js
+++ bang-olufsen/js/gnb_common.js
@@ -1,24 +1,46 @@
+
 checkHeader();
 $(window).on('scroll', function() {
     checkHeader();
 });
 
+
 function checkHeader() {
     var scrollAmt = $(document).scrollTop();
-    if (scrollAmt > 200) {
+    if (scrollAmt > 1000) {
         $('#header').addClass('white');
-        $(".support").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/icon_support_black.svg"); 
-        $(".store").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/icon_store_black.svg"); 
-        $(".cart").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/cart_black.svg");  
-        $(".primary-logo").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/primary_logo_black.svg"); 
+        $(".support").attr("src","img/icon_support_black.svg"); 
+        $(".store").attr("src","img/icon_store_black.svg"); 
+        $(".cart").attr("src","img/cart_black.svg");  
+        $(".primary-logo").attr("src","img/primary_logo_black.svg"); 
+        $(".menu.bars-btn span").css({"background-color": "#000"}); 
     } else {
         $('#header').removeClass('white');
-        $(".support").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/icon_support_white.svg");
-        $(".store").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/icon_store_white.svg"); 
-        $(".cart").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/cart-white.svg");  
-        $(".primary-logo").attr("src","https://yunafo3333.github.io/project/bang-olufsen/img/primary-logo-white.svg"); 
+        $(".support").attr("src","img/icon_support_white.svg");
+        $(".store").attr("src","img/icon_store_white.svg"); 
+        $(".cart").attr("src","img/cart-white.svg");  
+        $(".primary-logo").attr("src","img/primary-logo-white.svg"); 
+        $(".menu.bars-btn span").css({"background-color": "#fff"}); 
     }
 
-
-
 }
+
+
+$(function () {
+    $('.menu').on('click', function () {
+        $(this).toggleClass('on');
+
+        if ($('.menu').is('.on')) {
+            $('.side-gnb-container').stop().animate({
+                left: 0
+            });
+            $(".menu.bars-btn span").stop().css({"background-color": "#fff"}); 
+        } else {
+            $('.side-gnb-container').stop().animate({
+                left: '-100%'
+            });
+        }
+    });
+});
+
+
--- bang-olufsen/js/script.js
+++ bang-olufsen/js/script.js
@@ -1,7 +1,32 @@
-
-
-$(document).on('click', 'a[href="#"]', function(e) {
+$(document).on('click', 'a[href="#"]', function (e) {
     e.preventDefault();
 });
+
+
+$(function () {
+    $('.gnb-inner li:first-child').each(function () {
+        $(this).find('> a').append('<i class="fas fa-plus mobile"><span>열기/닫기</span></i>');
+        $(this).find('.fas').css({
+            'position': 'absolute',
+            'right': 0,
+            'top': '18px'
+        })
+    });
+    $('.gnb-inner li:first-child').on('click', function () {
+        $(this).toggleClass('on');
+        $('.gnb-inner').toggleClass('on');
+
+        if ($(this).hasClass('on')) {
+            $(this).find('> a > i').removeClass('fa-plus');
+            $(this).find('> a > i').addClass('fa-minus');
+        } else {
+            $(this).find('> a > i').addClass('fa-plus');
+            $(this).find('> a > i').removeClass('fa-minus');
+        }
+
+    });
+});
+
+
 
 
